39371b517b37a58a9890680d73c64fd695763ce9
Team Workspace Two <teamworkspace2@fssa.freshworks.com>
2024-07-27 18:54:51 +0530

    create a ENV files

--- src/main/java/com/fssa/todo/TodoAppApiApplication.java
+++ src/main/java/com/fssa/todo/TodoAppApiApplication.java
@@ -1,13 +1,23 @@
 package com.fssa.todo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoAppApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Set environment variables
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(TodoAppApiApplication.class, args);
+    }
+    // Load .env file
 
 }
